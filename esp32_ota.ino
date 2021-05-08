d11ab4407af9125cfd6d10c88ece7d928f5499f4
RishabhL1997 <49855611+RishabhL1997@users.noreply.github.com>
2021-05-08 13:14:59 +0530

    Update esp32_ota.ino

--- esp32_ota.ino
+++ esp32_ota.ino
@@ -4,8 +4,8 @@
 #include <WiFiClientSecure.h>
 #include "cert.h"
 
-const char * ssid = "home_wifi";
-const char * password = "helloworld";
+const char * ssid = "Poco F1";
+const char * password = "12345678";
 
 
 String FirmwareVer = {
